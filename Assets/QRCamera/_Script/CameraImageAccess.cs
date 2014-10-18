3caca02cd89939bd491111153d442aebe40652e7
Adrian Cuzman <adriancuzman@gmail.com>
2014-10-18 23:55:12 +0300

    added game enable logic

--- Assets/QRCamera/_Script/CameraImageAccess.cs
+++ Assets/QRCamera/_Script/CameraImageAccess.cs
@@ -48,6 +48,8 @@ public class CameraImageAccess : MonoBehaviour, ITrackerEventHandler {
 			
 			cameraFeed = CameraDevice.Instance.GetCameraImage(Image.PIXEL_FORMAT.GRAYSCALE);
 			tempText = new QRCodeReader().decode(cameraFeed.Pixels, cameraFeed.BufferWidth, cameraFeed.BufferHeight).Text;
+
+			tryEnableGame(tempText);
 		}
 		catch {
 			// Fail detecting QR Code!
@@ -55,6 +57,22 @@ public class CameraImageAccess : MonoBehaviour, ITrackerEventHandler {
 		finally {
 			if(!string.IsNullOrEmpty(tempText)) {
 				qrText = tempText;
+			}
+		}
+	}
+
+	private void tryEnableGame(string text){
+		if (text != null && text.Length > 0){
+			if (text.StartsWith("ArcadeCity-")){
+				Debug.Log("ACCEPTED CODE: "+text);
+				string[] tokens = text.Split('-');
+				if (tokens.Length > 1){
+					string gameName = tokens[1];
+
+					PlayerPrefs.SetInt(gameName, 1);
+
+					Application.LoadLevel("GamesScene");
+				}
 			}
 		}
 	}
